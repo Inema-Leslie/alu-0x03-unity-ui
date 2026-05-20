5f98a6650ff085da6590a2615e0ceaef145afddf
Inema_Leslie <a.inema2@alustudent.com>
2026-05-20 08:46:41 +0200

    winlose

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
-    private Rigidbody rb;
-    private int score = 0;
-    public int health = 5;
+    public float speed;
+    public int score;
+    public int health;
     public Text scoreText;
+    public Text healthText;  // ← new
 
-    
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        rb.AddForce(movement * speed);
-    }
-
-    void Update()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        rb.AddForce(movement * speed);
-    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
@@ -43,15 +20,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Trap"))
         {
             health--;
-            Debug.Log("Health:" + health);
-        }
-        if (other.gameObject.CompareTag("Goal"))
-        {
-            Debug.Log("You win!");
+            // Debug.Log(health);
+            SetHealthText();  // ← new
         }
     }
+
     void SetScoreText()
     {
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
     }
 }
